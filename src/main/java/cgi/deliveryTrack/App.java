7633795d6ea42b8081a8a9f67cc6d3ea5ad7e4ad
julien <julien.griffault90@gmail.com>
2019-01-07 11:24:21 +0100

    Initial commit

---
+++ src/main/java/cgi/deliveryTrack/App.java
@@ -0,0 +1,13 @@
+package cgi.deliveryTrack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
